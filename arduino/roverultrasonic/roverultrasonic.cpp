d45a088ab3607dc7806130e72084b1fc9d9421cb
Scott Umsted <scottumsted@gmail.com>
2017-08-22 12:32:13 -0500

    supporting 5 ultrasonic encoders

--- arduino/roverultrasonic/roverultrasonic.cpp
+++ arduino/roverultrasonic/roverultrasonic.cpp
@@ -7,14 +7,15 @@
 #define US_FRONT_ECHO_PIN 12
 #define US_LEFT_TRIG_PIN 3
 #define US_LEFT_ECHO_PIN 4
+#define US_LOW_TRIG_PIN 5
+#define US_LOW_ECHO_PIN 6
 
-#define US_LEFT_PIN 3
-#define US_LOW_PIN 5
-#define US_RIGHT_PIN 9
-#define US_REAR_PIN 7
+#define US_REAR_TRIG_PIN 7
+#define US_REAR_ECHO_PIN 8
+#define US_RIGHT_TRIG_PIN 9
+#define US_RIGHT_ECHO_PIN 10
+
 #define ENC_LEFT_FRONT 2
-
-#define SAFETY_CADENCE_MS 500  // millisecs
 
 #define MAX_BUFFER_SIZE 50
 
@@ -29,22 +30,18 @@ long right_distance=0;
 long rear_distance=0;
 long left_front_encoder=0;
 
-
-void safetyCheck() {
-    if(false){
-    } else {
-    }
-    ledVal = (ledVal == HIGH)?LOW:HIGH;
-    digitalWrite(LED_PIN, ledVal);
-}
-
-
 void doStep(byte step){
     // for debugging
     // Serial.println("step: "+String(step));
     // delay(1000);
 }
 
+void writeSensorData(){
+    char result[100];
+   sprintf(result,"{\"left\":%ld,\"low\":%ld,\"front\":%ld,\"right\":%ld,\"rear\":%ld}",
+       left_distance, low_distance, front_distance, right_distance, rear_distance);
+    Serial.println(result);
+}
 
 void serialHandler(){
     char readBuffer[MAX_BUFFER_SIZE] = "";
@@ -67,10 +64,8 @@ void serialHandler(){
                 Serial.write("{\"id\":\"encoder\"}");
                 break;
             default:
-                char result[100];
-                sprintf(result,"{\"left\":%d,\"low\":%d,\"front\":%d,\"right\":%d,\"rear\":%d,\"encoder\":%d}",
-                        left_distance, low_distance, front_distance, right_distance, rear_distance, left_front_encoder);
-                Serial.write(result);
+                writeSensorData();
+                break;
         }
     }
 }
@@ -92,11 +87,13 @@ void setup() {
     pinMode(US_FRONT_ECHO_PIN, INPUT);
     pinMode(US_LEFT_TRIG_PIN, OUTPUT);
     pinMode(US_LEFT_ECHO_PIN, INPUT);
-//    pinMode(US_LEFT_PIN, INPUT);
-//    pinMode(US_LOW_PIN, INPUT);
-//    pinMode(US_RIGHT_PIN, INPUT);
-//    pinMode(US_REAR_PIN, INPUT);
-//
+    pinMode(US_LOW_TRIG_PIN, OUTPUT);
+    pinMode(US_LOW_ECHO_PIN, INPUT);
+    pinMode(US_RIGHT_TRIG_PIN, OUTPUT);
+    pinMode(US_RIGHT_ECHO_PIN, INPUT);
+    pinMode(US_REAR_TRIG_PIN, OUTPUT);
+    pinMode(US_REAR_ECHO_PIN, INPUT);
+
 //    pinMode(ENC_LEFT_FRONT, INPUT);
 //    digitalWrite(ENC_LEFT_FRONT, HIGH);
 //    attachInterrupt(0, encoderCb, CHANGE);
@@ -112,29 +109,26 @@ int getDistance(int trigPin, int echoPin){
     digitalWrite(trigPin, LOW);
 
     pinMode(echoPin, INPUT);
-    duration = pulseIn(echoPin, HIGH,10000);
-
+    duration = pulseIn(echoPin, HIGH);
+    // duration = pulseIn(echoPin, HIGH,10000);
+    // Serial.println("duration: "+String(duration));
     if(duration < 0){
         return 0;
-    } else if(duration >= 10000){
-        return 162;
+    } else if(duration >= 10500){
+        return 180;
     } else {
         return (duration/2) / 29.1;
     }
 }
 
-void writeSensorData(){
-    char result[100];
-    sprintf(result,"{\"left\":%d,\"low\":%d,\"front\":%d,\"right\":%d,\"rear\":%d}",
-        left_distance, low_distance, front_distance, right_distance, rear_distance);
-    Serial.println(result);
-}
 
 void loop() {
     serialHandler();
     front_distance = getDistance(US_FRONT_TRIG_PIN, US_FRONT_ECHO_PIN);
+    low_distance = getDistance(US_LOW_TRIG_PIN, US_LOW_ECHO_PIN);
     left_distance = getDistance(US_LEFT_TRIG_PIN, US_LEFT_ECHO_PIN);
-    // Serial.println(left_distance);
-    writeSensorData();
-    delay(100);
+    right_distance = getDistance(US_RIGHT_TRIG_PIN, US_RIGHT_ECHO_PIN);
+    rear_distance = getDistance(US_REAR_TRIG_PIN, US_REAR_ECHO_PIN);
+    // writeSensorData();
+    // delay(500);
 }
