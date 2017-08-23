9079496b9a8108f2d0b5883decbd8147fdb09d6b
Scott Umsted <scottumsted@gmail.com>
2017-08-23 07:05:16 -0500

    setting up servo and ultrasonic as test harness for biy hall effect encoder

--- arduino/roverencoder/roverencoder.cpp
+++ arduino/roverencoder/roverencoder.cpp
@@ -8,15 +8,18 @@
 
 #define ENC_LEFT_FRONT 2
 
+#define SERVO_PIN 9
+
 #define MAX_BUFFER_SIZE 50
 
-
-bool commandProcessed = false; // check used by safety timer to tell if command issued
 byte ledVal = HIGH; // safety timer flips the led on and off
 
 long left_distance=0;
 
 long left_front_encoder=0;
+
+Servo theServo;
+int pos = 0;
 
 void doStep(byte step){
     // for debugging
@@ -28,11 +31,15 @@ void writeSensorData(){
     char result[100];
     sprintf(result,"{\"left\":%ld}",
        left_distance);
-    Serial.write(result);
+    Serial.println(result);
 }
 
 void writeId(){
-    Serial.write("{\"id\":\"ultrasonic\"}");
+    Serial.println("{\"id\":\"ultrasonic\"}");
+}
+
+void moveServo(int posIn){
+    theServo.write(posIn);
 }
 
 void serialHandler(){
@@ -60,13 +67,13 @@ void serialHandler(){
                 break;
             case 'M':
                 char pulseBuffer[5];
+                int posIn;
                 memcpy(pulseBuffer, readBuffer+1, 4);
                 pulseBuffer['\0'];
-                int pulse = atoi(pulseBuffer);
-
-
+                posIn = atoi(pulseBuffer);
+                moveServo(posIn);
             default:
-                writeUnknown();
+                writeId();
                 break;
         }
     }
@@ -102,17 +109,10 @@ void setup() {
     while(!Serial){}
     writeId();
     pinMode(LED_PIN, OUTPUT);
-    pinMode(US_FRONT_TRIG_PIN, OUTPUT);
-    pinMode(US_FRONT_ECHO_PIN, INPUT);
     pinMode(US_LEFT_TRIG_PIN, OUTPUT);
     pinMode(US_LEFT_ECHO_PIN, INPUT);
-    pinMode(US_LOW_TRIG_PIN, OUTPUT);
-    pinMode(US_LOW_ECHO_PIN, INPUT);
-    pinMode(US_RIGHT_TRIG_PIN, OUTPUT);
-    pinMode(US_RIGHT_ECHO_PIN, INPUT);
-    pinMode(US_REAR_TRIG_PIN, OUTPUT);
-    pinMode(US_REAR_ECHO_PIN, INPUT);
 
+    theServo.attach(SERVO_PIN);
 //    pinMode(ENC_LEFT_FRONT, INPUT);
 //    digitalWrite(ENC_LEFT_FRONT, HIGH);
 //    attachInterrupt(0, encoderCb, CHANGE);
@@ -120,15 +120,28 @@ void setup() {
 
 void loop() {
     serialHandler();
-    front_distance = getDistance(US_FRONT_TRIG_PIN, US_FRONT_ECHO_PIN);
-    low_distance = getDistance(US_LOW_TRIG_PIN, US_LOW_ECHO_PIN);
     left_distance = getDistance(US_LEFT_TRIG_PIN, US_LEFT_ECHO_PIN);
-    right_distance = getDistance(US_RIGHT_TRIG_PIN, US_RIGHT_ECHO_PIN);
-    rear_distance = getDistance(US_REAR_TRIG_PIN, US_REAR_ECHO_PIN);
-    ledVal = (ledVal==HIGH) ? LOW : HIGH;
+
+    if(left_distance < 50){
+        moveServo(100);
+    } else {
+        moveServo(90);
+    }
+
     delay(100);
+    ledVal = (ledVal==HIGH) ? LOW : HIGH;
     digitalWrite(LED_PIN, ledVal);
 
     // writeSensorData();
     // delay(200);
+
+    // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    //     // in steps of 1 degree
+    //     theServo.write(pos);              // tell servo to go to position in variable 'pos'
+    //     delay(15);                       // waits 15ms for the servo to reach the position
+    // }
+    // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    //     theServo.write(pos);              // tell servo to go to position in variable 'pos'
+    //     delay(15);                       // waits 15ms for the servo to reach the position
+    // }
 }
