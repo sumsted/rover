f11ee109679b54e6a85e28c498cebcdf3fc06165
Scott Umsted <scottumsted@gmail.com>
2017-08-26 08:34:47 -0500

    using both interrupts for encoders, includes test code

--- arduino/roverencoder/roverencoder.cpp
+++ arduino/roverencoder/roverencoder.cpp
@@ -2,30 +2,33 @@
 #include <Servo.h>
 
 #define LED_PIN 13
-#define LEFT_LED_PIN 11
-#define RIGHT_LED_PIN 12
 
-#define US_LEFT_TRIG_PIN 3
-#define US_LEFT_ECHO_PIN 4
-
-#define ENC_LEFT_FRONT 2
-#define ENC_RIGHT_FRONT 6
-
-#define LEFT_SERVO_PIN 9
-#define RIGHT_SERVO_PIN 7
+#define LEFT_FRONT_ENCODER 3
+#define RIGHT_FRONT_ENCODER 2
 
 #define MAX_BUFFER_SIZE 50
 
-byte ledVal = HIGH; // safety timer flips the led on and off
+byte ledVal = HIGH; 
 
-long leftDistance=0;
 long leftFrontEncoder=0;
+long leftLastMillis=0;
+long leftLastRotations=0;
 
-long rightDistance=0;
 long rightFrontEncoder=0;
+long rightLastMillis=0;
+long rightLastRotations=0;
 
+// Test code
+#define LEFT_SERVO_PIN 9
+#define RIGHT_SERVO_PIN 7
 Servo leftServo;
 Servo rightServo;
+// #define LEFT_LED_PIN 11
+// #define RIGHT_LED_PIN 12
+// #define LEFT_US_TRIG_PIN 6
+// #define LEFT_US_ECHO_PIN 4
+
+// long counter = 0;
 
 int pos = 0;
 
@@ -37,15 +40,14 @@ void doStep(byte step){
 
 void writeSensorData(){
     char result[100];
-    sprintf(result,"{\"left\":%ld,\"rightt\":%ld}",
-       leftDistance, rightDistance);
+    sprintf(result,"{\"left\":%ld,\"right\":%ld}",
+       leftFrontEncoder, rightFrontEncoder);
     Serial.println(result);
 }
 
 void writeId(){
-    Serial.println("{\"id\":\"ultrasonic\"}");
+    Serial.println("{\"id\":\"encoder\"}");
 }
-
 
 void serialHandler(){
     char readBuffer[MAX_BUFFER_SIZE] = "";
@@ -67,16 +69,9 @@ void serialHandler(){
             case 'I':
                 writeId();
                 break;
-            case 'U':
+            case 'E':
                 writeSensorData();
                 break;
-            case 'M':
-                char pulseBuffer[5];
-                int posIn;
-                memcpy(pulseBuffer, readBuffer+1, 4);
-                pulseBuffer['\0'];
-                posIn = atoi(pulseBuffer);
-                leftServo.write(posIn);
             default:
                 writeId();
                 break;
@@ -84,35 +79,15 @@ void serialHandler(){
     }
 }
 
-void encoderCb(){
+void leftEncoderCb(){
     leftFrontEncoder++;
+}
+
+void rightEncoderCb(){
     rightFrontEncoder++;
 }
 
-int getDistance(int trigPin, int echoPin){
-    long duration = 0;
-
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(5);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-    pinMode(echoPin, INPUT);
-    // duration = pulseIn(echoPin, HIGH, 8730);
-    duration = pulseIn(echoPin, HIGH);
-    // Serial.println("duration: "+String(duration));
-    delay(100);
-    if(duration < 0){
-        return 0;
-    } else if(duration >= 8730 || duration == 0){
-        return 150;
-    } else {
-        return (duration/2) / 29.1;
-    }
-}
-
-long writeRpm(long ticks, long *prevMillis, long *prevRotations){
+long writeRpm(char label, long ticks, long *prevMillis, long *prevRotations){
     long currentMillis = millis();
     long currentRotations = ticks/2;
     char buffer[200];
@@ -124,7 +99,7 @@ long writeRpm(long ticks, long *prevMillis, long *prevRotations){
     // sprintf(buffer, "%f = %f / %f", rpm, rotations, minutes);
     *prevMillis = currentMillis;
     *prevRotations = currentRotations;
-    Serial.println(String(rpm)+buffer);
+    Serial.println(String(label)+": "+String(rpm)+buffer);
     return rpm;
 }
 
@@ -134,68 +109,81 @@ void setup() {
     writeId();
     pinMode(LED_PIN, OUTPUT);
 
+    pinMode(LEFT_FRONT_ENCODER, INPUT);
+    digitalWrite(LEFT_FRONT_ENCODER, HIGH);
     
-    pinMode(US_LEFT_TRIG_PIN, OUTPUT);
-    pinMode(US_LEFT_ECHO_PIN, INPUT);
+    pinMode(RIGHT_FRONT_ENCODER, INPUT);
+    digitalWrite(RIGHT_FRONT_ENCODER, HIGH);
 
+    attachInterrupt(digitalPinToInterrupt(LEFT_FRONT_ENCODER), leftEncoderCb, RISING);
+    attachInterrupt(digitalPinToInterrupt(RIGHT_FRONT_ENCODER), rightEncoderCb, RISING);
+
+    // more test code
     leftServo.attach(LEFT_SERVO_PIN);
-    pinMode(LEFT_LED_PIN, OUTPUT);
-    pinMode(ENC_LEFT_FRONT, INPUT);
-    digitalWrite(ENC_LEFT_FRONT, HIGH);
-    
     rightServo.attach(RIGHT_SERVO_PIN);
-    pinMode(RIGHT_LED_PIN, OUTPUT);
-    pinMode(ENC_RIGHT_FRONT, INPUT);
-    digitalWrite(ENC_RIGHT_FRONT, HIGH);
-
-    attachInterrupt(0, encoderCb, RISING);
+    leftServo.write(110);
+    rightServo.write(120);
+    // pinMode(LEFT_US_TRIG_PIN, OUTPUT);
+    // pinMode(LEFT_US_ECHO_PIN, INPUT);
+    // pinMode(LEFT_LED_PIN, OUTPUT);
+    // pinMode(RIGHT_LED_PIN, OUTPUT);
 }
 
-long counter = 0;
-long leftLastMillis=0;
-long leftLastRotations=0;
+
 
 void loop() {
     serialHandler();
-    leftDistance = getDistance(US_LEFT_TRIG_PIN, US_LEFT_ECHO_PIN);
-
-    if(leftDistance < 50){
-        // float ratio = (50.0 - (float)leftDistance) / 50.0;
-        // int pos = ratio * 40 + 110;
-        // moveServo(pos);
-        leftServo.write(90);
-        rightServo.write(120);
-    } else {
-        leftServo.write(90);
-        rightServo.write(90);
-    }
-
     ledVal = (ledVal==HIGH) ? LOW : HIGH;
     digitalWrite(LED_PIN, ledVal);
-
-    counter++;
-    // if(!(counter%20)){
-    //     writeRpm(leftFrontEncoder, leftLastMillis, leftLastRotations);
-    //     writeRpm(rightFrontEncoder, rightLastMillis, rightLastRotations);
-    // }
-    Serial.println("L: "+String(leftFrontEncoder)+", "+String((float)leftFrontEncoder/2.0));
-    Serial.println("R: "+String(rightFrontEncoder)+", "+String((float)rightFrontEncoder/2.0));
-
-    int leftLight = (!((leftFrontEncoder) % 2)) ? HIGH : LOW;
-    digitalWrite(LEFT_LED_PIN, leftLight);
-
-    int rightLight = (!((rightFrontEncoder) % 2)) ? HIGH : LOW;
-    digitalWrite(RIGHT_LED_PIN, rightLight);
-    // writeSensorData();
-    // delay(200);
-
-    // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    //     // in steps of 1 degree
-    //     leftServo.write(pos);              // tell servo to go to position in variable 'pos'
-    //     delay(15);                       // waits 15ms for the servo to reach the position
-    // }
-    // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    //     leftServo.write(pos);              // tell servo to go to position in variable 'pos'
-    //     delay(15);                       // waits 15ms for the servo to reach the position
-    // }
 }
+
+// test code using ultrasonic sensor and two servos
+// int getDistance(int trigPin, int echoPin){
+//     long duration = 0;
+
+//     digitalWrite(trigPin, LOW);
+//     delayMicroseconds(5);
+//     digitalWrite(trigPin, HIGH);
+//     delayMicroseconds(10);
+//     digitalWrite(trigPin, LOW);
+
+//     pinMode(echoPin, INPUT);
+//     // duration = pulseIn(echoPin, HIGH, 8730);
+//     duration = pulseIn(echoPin, HIGH);
+//     // Serial.println("duration: "+String(duration));
+//     delay(100);
+//     if(duration < 0){
+//         return 0;
+//     } else if(duration >= 8730 || duration == 0){
+//         return 150;
+//     } else {
+//         return (duration/2) / 29.1;
+//     }
+// }
+// void testEncoders(){
+//     if(leftDistance < 50){
+//         leftServo.write(110);
+//         rightServo.write(120);
+//     } else {
+//         leftServo.write(90);
+//         rightServo.write(90);
+//     }
+
+//     ledVal = (ledVal==HIGH) ? LOW : HIGH;
+//     digitalWrite(LED_PIN, ledVal);
+
+//     counter++;
+//     if(!(counter%100)){
+//         writeRpm('L', leftFrontEncoder, &leftLastMillis, &leftLastRotations);
+//         writeRpm('R', rightFrontEncoder, &rightLastMillis, &rightLastRotations);
+//     }
+//     Serial.println("L: "+String(leftFrontEncoder)+", "+String((float)leftFrontEncoder/2.0));
+//     Serial.println("R: "+String(rightFrontEncoder)+", "+String((float)rightFrontEncoder/2.0));
+
+//     int leftLight = (!((leftFrontEncoder) % 2)) ? HIGH : LOW;
+//     digitalWrite(LEFT_LED_PIN, leftLight);
+
+//     int rightLight = (!((rightFrontEncoder) % 2)) ? HIGH : LOW;
+//     digitalWrite(RIGHT_LED_PIN, rightLight);
+
+// }
