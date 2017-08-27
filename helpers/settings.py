f491db621633c885f7838135fcd8c920d4691d72
Scott Umsted <scottumsted@gmail.com>
2017-08-27 08:40:27 -0500

    usb port id

--- helpers/settings.py
+++ helpers/settings.py
@@ -18,7 +18,7 @@ def get_usb_device(name, default='/dev/ttyUSB0', device_id=None):
                 address = '/dev/'+line.split(' ')[-1]
                 if device_id is not None:
                     device = serial.Serial(address, 9600, timeout=.5)
-                    device.write('I!')
+                    device.write('I!'.encode())
                     result = device.readLine().decode()
                     print('result: %s' % result)
                     device_info = json.loads(result)
